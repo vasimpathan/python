da350e31fafbc8fd3b95027106ab7447ae167948
vasimrp2 <vasimrp@gmail.com>
2024-12-28 22:17:30 +0530

    added new file

---
+++ Day_28.py
@@ -0,0 +1,16 @@
+# --- Data Science ---
+# Numpy, Pandas, Matplotlib
+# Sklearn, Pytorch, Tensorflow
+
+# --- APIs ---
+# Requests, Flask, FastAPI
+
+# --- Web Development ---
+# Flask, Streamlit, Django
+
+# --- Web Scraping / Web Crawling ---
+# Selenium, BeautifulSoup, Scrapy
+
+# --- Computer Vision / Image Processing ---
+# OpenCV, Pillow, MoviePy, Ultralytics
+
---
+++ Day_29.py
@@ -0,0 +1,14 @@
+# Popular Python Resources
+
+# --- Reading
+# W3Schools - https://www.w3schools.com/python/
+# GeeksforGeeks - https://www.geeksforgeeks.org/python-programming-language-tutorial/
+# TutorialsPoint - https://www.tutorialspoint.com/python/
+
+# --- Testing
+# Hackerrank - https://www.hackerrank.com/domains/python
+# Leetcode - https://leetcode.com/problemset/
+# CodeChef - https://www.codechef.com/
+
+# --- Youtube
+# TheNewBoston
