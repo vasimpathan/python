5250970eaf671dd3e02b656b9d0c40335a13bc72
vasimrp2 <vasimrp@gmail.com>
2024-12-28 22:07:57 +0530

    Pandas file created

--- pandas.py
+++ pandas.py
@@ -3,3 +3,9 @@ import pandas as pd
 # Age: 20, 30, 40
 
 d = {'People': ['P1', 'P2', 'P3'], 'Age': [20, 30, 40]}
+
+df = pd.DataFrame(d)
+
+# pd.read_excel("score_updated.xlsx")
+# pd.read_csv("sample.csv")
+print(df)
