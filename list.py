c09d34e01a5ffdecdb3547c4708fcbb47d4da413
vasimrp2 <vasimrp@gmail.com>
2025-07-29 18:41:40 +0530

    List Data Type

--- list.py
+++ list.py
@@ -1,3 +1,5 @@
+# List is the ordered collection of element enclosed within square brancket []
+# It is mutable means we can change value in it [Elements are not permanant]
 item = ['vasim',"wajid","azim","1","2","3"]
 
 print("Print all the element from the list")
@@ -26,7 +28,29 @@ print(item)
 print("\nTo remove the element from the list - remove")
 item.remove("pqr")
 print(item)
+item.pop()
+print(item)
 
 print("\nTo clear or empty the list - clear")
 item.clear()
 print(item)
+
+#Revering element
+a = [2,1,3,5,4,6]
+b = ['d','c','b','a']
+a.reverse()
+print(f"Reverse Element : {a}")
+a.sort()
+print(f"Ascending Sort : {a}")
+b.sort()
+print(f"Ascending Sort : {b}")
+
+#Insert Element to specified index
+a.insert(1,"Vasim")
+print(f"Newly Added Element at index 1 : {a}")
+
+# Concatenate
+print(a + b)
+
+#Repeatation
+print(b*3)
